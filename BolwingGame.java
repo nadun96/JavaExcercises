013594277d21bab73b2fc16749a060253c31811a
nadun96 <56214142+nadun96@users.noreply.github.com>
2022-07-20 10:38:18 +0530

    Update BolwingGame.java

--- BolwingGame.java
+++ BolwingGame.java
@@ -1,3 +1,21 @@
+/*
+Bowling Game
+
+
+You are creating a bowling game!
+The given code declares a Bowling class with its constructor and addPlayer() method.
+Each player of the game has a name and points, and are stored in the players HashMap.
+The code in main takes 3 players data as input and adds them to the game.
+You need to add a getWinner() method to the class, which calculates and outputs the name of the player with the maximum points.
+
+Sample Input:
+Dave 42
+Amy 103
+Rob 64
+
+Sample Output:
+Amy
+*/
 import java.util.*; 
 
 public class Bowling {
